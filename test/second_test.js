a0c6519a27f5f5012e4c06226aa9ac67c1a65a22
MaximSiliyan <jmsiliyan@gmail.com>
2020-06-10 03:48:44 +0800

    The test 2_3 was added

--- test/second_test.js
+++ test/second_test.js
@@ -10,6 +10,14 @@ describe('exercise_2', () => {
     await driver.wait(until.titleIs("Новости проекта"),2000);
   });
 
+  it('exercise_2_3', async () => {
+      await driver.get('http://www.litecart.com/admin/');
+      await driver.findElement(By.name('username')).sendKeys('admin');
+      await driver.findElement(By.name('password')).sendKeys('admin');
+      await driver.findElement(By.name('login')).click();
+      await driver.wait(until.elementLocated(By.css('.logotype')),2000);
+  });
+
   after( async () => {
     driver.quit()
   });
